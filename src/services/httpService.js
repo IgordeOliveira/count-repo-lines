e5f78599019e943535bb13eda013bd5e6e8bb8fb
Igor Oliveira <igor.oliveira@tegrus.com.br>
2020-08-01 14:55:33 -0300

    add Redis to cache

---
+++ src/services/httpService.js
@@ -0,0 +1,26 @@
+const { setup, RedisStore } = require('axios-cache-adapter')
+const redis = require('redis')
+// setup redis client
+const client = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+})
+const store = new RedisStore(client)
+
+// Create `axios` instance with cache adapter
+const httpService = setup({
+  // `axios` options
+  baseURL: 'https://github.com',
+  // `axios-cache-adapter` options
+  cache: {
+    // Tell adapter to attempt using response headers
+    readHeaders: true,
+    // 30 min of cache if dont find headers
+    maxAge: 30 * 60 * 1000,
+    // add redis as store for cache
+    store
+  }
+})
+
+module.exports = { client: httpService }
