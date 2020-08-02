6fd4d41910013e36700bfdf9124611f9ae68effc
Igor Oliveira <igor.oliveira@tegrus.com.br>
2020-08-02 02:19:53 -0300

    change repo for test

--- test/api.test.js
+++ test/api.test.js
@@ -5,7 +5,7 @@ const app = require('../src/app');
 describe('GET /api/repo-count-lines', () => {
   it('responds with a json message', (done) => {
     request(app)
-      .get('/api/repo-count-lines?repo=https://github.com/laravel/horizon')
+      .get('/api/repo-count-lines?repo=https://github.com/IgordeOliveira/count-repo-lines')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, done);
